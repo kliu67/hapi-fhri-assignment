f80a5f49f5191321a6df84f340c78151619b6b02
kliu67 <47793951+kliu67@users.noreply.github.com>
2021-12-16 15:47:09 -0500

    Intermediate tasks
    Implemented methods to accept a local file that contains a list of family names, and performs search for each family name. The response time is recorded. At the end of each run, the average response time is calculated by dividing the total response time by the number of search requests. 
    
    Default number of runs is 3, but can be changed by including an argument when running the program.
    Cache is turned on for the 1st and 2nd runs, and turned off for the 3rd run. This is done by toggling Cache control in the client program. There should be a more elegant way of toggling the cache(perhaps in the get message), but I was unable to program it in time.

--- playground-basic-master/src/main/java/SampleClient.java
+++ playground-basic-master/src/main/java/SampleClient.java
@@ -1,6 +1,6 @@
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.api.CacheControlDirective;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
-import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -8,42 +8,93 @@ import java.util.Comparator;
 import java.util.List;
 
 import org.hl7.fhir.r4.model.Bundle;
-import org.hl7.fhir.r4.model.HumanName;
 import org.hl7.fhir.r4.model.Patient;
-import org.hl7.fhir.r4.model.StringType;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 
 public class SampleClient {
 
     public static void main(String[] theArgs) {
 
         // Create a FHIR client
-    	System.out.println("running started");
         FhirContext fhirContext = FhirContext.forR4();
         IGenericClient client = fhirContext.newRestfulGenericClient("http://hapi.fhir.org/baseR4");
-        client.registerInterceptor(new LoggingInterceptor(false));
+        client.registerInterceptor(new myClientInterceptor(false));
+        //responseTime for each search
+        int totalResponseTime = 0;
+        //total number of runs completed
+        int numberOfRuns = 0; 
+        //counter for number of loops to run
+        int loopCounter = Integer.valueOf(theArgs[0]);
+        //toggles cache-control for the httprequest
+        boolean cacheOff = false;
+        //open a file containing a list of family names
+        while(loopCounter > 0) {
+        	System.out.println("Loop " + loopCounter);
+        	System.out.println("Searching...");
+        	try {
+        		//opens a .txt file containing family names
+            	Scanner scanner = new Scanner(new File("src\\main\\java\\FamilyNames.txt"));
+            	//repeats the search n number of times, n = loopCounter
+    			while (scanner.hasNextLine()) {
+    				//list to holder the search result
+    	            
+    	            
+    				List<Patient> pList = new ArrayList<Patient>();
+    				
+    				//if this is a 3rd run, turn off cache
+    				if(loopCounter == 1) {
+    					cacheOff = true;
+    				}
+    				//if this is not a 3rd run, turn on cache
+    				else {
+    					cacheOff = false;
+    				}
+    				
+    				Bundle re = searchPatient(scanner.nextLine(), client, cacheOff);
+    				//sums the response time and number of search requests
+    				totalResponseTime += ((myClientInterceptor)client.getInterceptorService().getAllRegisteredInterceptors().get(0)).gettime();
+    				numberOfRuns++;
+    				
+    				//uncomment below to store the search result in an arralist, perform sort, and print the patient names to console
+//    				for(int i = 0; i < re.getEntry().size(); i++) {
+//    					pList.add((Patient)re.getEntry().get(i).getResource());
+//    				}
+//    				sortPatient(pList);
+//    				printPatient(pList);
+    				
+    			}
+    			scanner.close();
+    			
+    		} catch (FileNotFoundException e) {
+    			e.printStackTrace();
+    		}
+        	
+        	//outputs running results
+        	System.out.println("cache turnedd off ?: " + cacheOff);
+            System.out.println("total time is " + totalResponseTime);
+            System.out.println("numberofruns is " + numberOfRuns);
+            System.out.println("average time is " + avgTime(totalResponseTime, numberOfRuns));
+            loopCounter--;
+        }
 
-        // Search for Patient resources
-        Bundle response = client
+    }
+    
+    
+    public static Bundle searchPatient (String familyName, IGenericClient client, boolean cacheControl) {
+    	return client
                 .search()
                 .forResource("Patient")
-                .where(Patient.FAMILY.matches().value("SMITH"))
+                .where(Patient.FAMILY.matches().value(familyName))
+                .cacheControl(new CacheControlDirective().setNoCache(cacheControl))
                 .returnBundle(Bundle.class)
                 .execute();
-  
-       
-        List<Patient> patientList = new ArrayList<Patient>(); //create a list storing patients' information
-        for(int i = 0; i < response.getEntry().size(); i++) {
-        	patientList.add((Patient)response.getEntry().get(i).getResource());
-        }
-        
-        sortPatients(patientList);
-        printPatient(patientList);
-        System.out.println("exit");
-   
+    	
     }
-    //BASIC TASKS
-    public static void sortPatients(List<Patient> patients) {  //Sorts a list of `Patient` by Given Name
-    	System.out.println("sorting patients");
+    
+    public static void sortPatient(List<Patient> patients) {  //Sorts a list of `Patient` by Given Name
     	Collections.sort(patients, new Comparator<Patient>() {  //Compares patient elements by the given name field
     	    public int compare(Patient p1, Patient p2) {
     	        return p1.getName().get(0).getGiven().get(0).toString().toUpperCase().compareTo(p2.getName().get(0).getGiven().get(0).toString().toUpperCase());
@@ -52,11 +103,14 @@ public class SampleClient {
     }
     
     public static void printPatient(List<Patient> patients) { //output a list of `Patient` to the console
-    	System.out.println("printing patients");
     	 for(Patient p : patients) {
          	System.out.println(p.getName().get(0).getGiven().get(0).toString() + " " + p.getName().get(0).getFamily().toString());
          	
          }
+    }
+    
+    public static int avgTime(int totalTime, int numberOfRuns) { //get the average response time of all searches in one iteration
+    	return totalTime / numberOfRuns;
     }
 
 }
